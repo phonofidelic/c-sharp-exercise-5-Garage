bff83bc8fce101203cafdffc2c7ba77ce512d7cc
Christopher Clemons <christopher@MacBookPro.lan>
2025-11-28 11:37:03 +0100

    remove unused code

--- Garage/CreateGarageMenu.cs
+++ Garage/CreateGarageMenu.cs
@@ -12,8 +12,7 @@ namespace Garage
     internal class CreateGarageMenu : IRender
     {
         public IApplicationRequest _request;
-        private ILogger<CreateGarageMenu> _logger;
-
+        private readonly ILogger<CreateGarageMenu> _logger;
         public CreateGarageMenu(IApplicationRequest request, ILogger<CreateGarageMenu> logger)
         {
             _request = request;
@@ -24,13 +23,12 @@ namespace Garage
         {
             _ = RenderAsync();
         }
+        
         private async Task RenderAsync()
         {
-            ConsoleUI.WriteLine("NewGarage rendered");
             CancellationToken stoppingToken = new();
-            CreateGarageRequestEvent garageCreatedEvent = new(new("My new garage", 50));
+            CreateGarageRequestEvent garageCreatedEvent = new(new CreateGarageRequestDTO("My new garage", 50));
             await _request.Publish(garageCreatedEvent, stoppingToken);
-            //ConsoleUI.WriteLine($"New garage created response: {garageCreatedEvent.Response}");
             _logger.LogInformation("New garage created response: {Response}", garageCreatedEvent.Response);
         }
     }
