3b0285339ed1d3cfdc9de78a047b0d22fbeb2a50
Christopher Clemons <christopher@MacBookPro.lan>
2025-12-02 01:25:25 +0100

    remove async methods in ListVehiclesView

--- Garage/ListVehiclesView.cs
+++ Garage/ListVehiclesView.cs
@@ -1,43 +1,19 @@
 ﻿using Garage.Library;
 using Garage.UI;
-using Microsoft.VisualStudio.Threading;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Garage
 {
     internal class ListVehiclesView(
-        IApplicationRequest request,
-         ListVehiclesMenu listParkedVehiclesMenu) : IRender, IRenderAsync
+        IApplicationRequest request) 
+        : IRender
     {
         public void Render()
         {
-            // CancellationToken stoppingToken = new();
-            // _ = RequestVehiclesAsync(stoppingToken);
-            
             bool result = request.TryPublish(new ListParkedVehiclesRequestEvent(
                new ListParkedVehiclesDTO([])));
 
             if (!result)
                 throw new Exception("Could not publish event");
-
-            listParkedVehiclesMenu.Render();
-        }
-
-        public Task RenderAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        private async Task RequestVehiclesAsync(CancellationToken stoppingToken)
-        {
-            
-            await request.PublishAsync(new ListParkedVehiclesRequestEvent(
-               new ListParkedVehiclesDTO([])), stoppingToken);
-            listParkedVehiclesMenu.Render();
         }
     }
 }
